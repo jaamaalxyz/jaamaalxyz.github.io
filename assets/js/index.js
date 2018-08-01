70783a8a2893e1f77a3670ec99b86efe573a2bc2
jamal-pb95 <jamal.pb95@gmail.com>
2018-08-01 10:54:55 +0600

    var changed to const

--- assets/js/index.js
+++ assets/js/index.js
@@ -57,8 +57,8 @@ $('a[href*="#"]')
 
   // Google Map
   function initMap() {
-    var uluru = {lat: 23.7817705, lng: 90.4152912};
-    var map = new google.maps.Map(
+    const uluru = {lat: 23.7817705, lng: 90.4152912};
+    const map = new google.maps.Map(
         document.getElementById('map'), {zoom: 15, center: uluru});
-    var marker = new google.maps.Marker({position: uluru, map: map, animation:google.maps.Animation.BOUNCE});
+    const marker = new google.maps.Marker({position: uluru, map: map, animation:google.maps.Animation.BOUNCE});
   }
