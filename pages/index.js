1cd1ac21a006fa188cc89ed54e84a699ae8145b5
Md. Jamal Uddin <jamal.pb95@gmail.com>
2021-05-30 02:18:51 +0600

    update title

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Jamal's Official Website</a>
         </h1>
 
         <p className="description">
