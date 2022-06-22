1f52f77a4f64d16b34c37ebb300059f13c088609
Md. Jamal Uddin <jamal.pb95@gmail.com>
2022-06-23 00:13:14 +0600

    add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,115 @@
-import { Box, Heading, Link, Stack, Text } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import NextLink from 'next/link';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>
-          Digital Garden of a self-taught Software Developer - Md. Jamal Uddin
-          (@jaamaal)
-        </title>
+        <title>Md. Jamal Uddin a.k.a jaamaal95</title>
         <meta
           name="description"
-          content="Digital Garden of a self-taught Software Developer - Md.
-          Jamal Uddin (@jaamaal)"
+          content="Personal site of Md. Jamal Uddin a.k.a jaamaal95"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles.container}>
+        <header>
+          <h1>Md. Jamal Uddin</h1>
+          <p style={{ paddingLeft: 3 }}>@jaamaal95</p>
+          <h3>Learner - Software Developer - Content Writer</h3>
+          <p>I built software and writing articles for humans</p>
+          <p>
+            I mostly work on <span className={styles.tag}>React</span>,{' '}
+            <span className={styles.tag}>React Native</span>,{' '}
+            <span className={styles.tag}>Next.js</span>,{' '}
+            <span className={styles.tag}>TypeScript</span>,{' '}
+            <span className={styles.tag}>Node.js</span>, and{' '}
+            <span className={styles.tag}>Databases</span>
+          </p>
+        </header>
+        <main>
+          <h2>Links</h2>
+          <ul>
+            <li>
+              <Link
+                href="https://www.linkedin.com/in/jaamaal95/"
+                target={'_blank'}
+              >
+                LinkedIn
+              </Link>
+            </li>
+            <li>
+              <Link href="https://github.com/jaamaal95" target={'_blank'}>
+                GitHub
+              </Link>
+            </li>
+            <li>
+              <Link href="https://twitter.com/jaamaal95" target={'_blank'}>
+                Twitter
+              </Link>
+            </li>
+            <li>
+              <Link href="https://jaamaal95.medium.com/" target={'_blank'}>
+                Medium
+              </Link>
+            </li>
+            <li>
+              <Link href="https://dev.to/jaamaal95" target={'_blank'}>
+                Dev.to
+              </Link>
+            </li>
+            <li>
+              <Link href="https://www.facebook.com/jaamaal95" target={'_blank'}>
+                Facebook
+              </Link>
+            </li>
+          </ul>
 
-      <Stack
-        as="main"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        h={['100%', '90vh']}
-      >
-        <Text fontSize={['2xl', '3xl', '5xl']}>👋 This is Md. Jamal Uddin</Text>
-        <Text fontSize={['lg', '2xl', '3xl']}>
-          I am a self-taught{' '}
-          <Box as="span" textColor={'blue.600'}>
-            <NextLink href="https://github.com/jaamaal95" passHref>
-              Software Developer
-            </NextLink>{' '}
-          </Box>
-          and{' '}
-          <Box as="span" textColor={'blue.600'}>
-            <NextLink href="/blog" passHref>
-              Technical Content Writer
-            </NextLink>
-          </Box>
-        </Text>
-        <Heading size="xl">Welcome to my Digital Garden</Heading>
-        <p>I built web application using TypeScript, React.js and Node.js</p>
-        <p>I also built cross platform mobile application using React Native</p>
-      </Stack>
-      <Stack
-        as="footer"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Text>
-          Built with 💖 by &nbsp;
-          <Link href="https://nextjs.org/docs/">Next.js</Link>
-        </Text>
-      </Stack>
+          <h2>Side Projects</h2>
+          <ul>
+            <li>
+              <Link
+                href="https://github.com/jaamaal95/AmalTracker"
+                target={'_blank'}
+              >
+                Building AmalTracker Mobile App using{' '}
+                <span className={styles.tag}>React Native</span> for my wife
+                {"'"}s community
+              </Link>
+            </li>
+          </ul>
+
+          <h2>Experiences</h2>
+          <ul>
+            <li>
+              <Link>National Polymer Group</Link>
+              <p>Software Developer</p>
+              <p>Jan, 2020 - Current</p>
+            </li>
+            <li>
+              <Link>National Polymer Industries Ltd.</Link>
+              <p>Junior Executive</p>
+              <p>Mar, 2018 - Dec, 2019</p>
+            </li>
+            <li>
+              <Link>NPOLY Factory, Gazipur, Tongi</Link>
+              <p>Assistant Executive</p>
+              <p>Oct, 2016 - Feb, 2018</p>
+            </li>
+          </ul>
+        </main>
+        <footer className={styles.footer}>
+          <p>Copyright @ {new Date().getFullYear()} - Md. Jamal Uddin</p>
+          <p>
+            Built with 💖 by &nbsp;
+            <Link href="https://nextjs.org/" target={'_blank'}>
+              Next.js
+            </Link>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 };
--- pages/tags/[tags].tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Tags = () => {
-  return <div>Tags</div>;
-};
-
-export default Tags;
