196d71810403fe381008b6299e3890e8e8387974
Md. Jamal Uddin <jamal.pb95@gmail.com>
2022-08-09 20:42:26 +0600

    add philoshopies;

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import FooterLink from './FooterLink';
 
 export const Footer: React.FC = () => (
-  <footer className="w-full lg:w-5/8 border-t border-teal-400">
+  <footer className="lg:w-5/8 border-t border-teal-400">
     <p className="text-center text-xs md:text-lg py-3">
       Powered by <FooterLink href="https://nextjs.org/" title="Next.js" /> and
       Deployed on <FooterLink href="https://vercel.com/" title="Vercel" />
