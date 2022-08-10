49480558050719d2b7021f5156e9ad3cf955daf6
Md. Jamal Uddin <jamal.pb95@gmail.com>
2022-08-10 11:18:42 +0600

    add react hydration solution

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+
+  if (typeof window === 'undefined') {
+    return <></>;
+  }
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
