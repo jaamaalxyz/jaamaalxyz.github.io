23ad80c42ceea2d9f64c9762b5c030d80c54f0e7
Md. Jamal Uddin <jamal.pb95@gmail.com>
2022-08-10 12:42:55 +0600

    add works

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Philosophies from './Philosophies';
+import Works from './Works';
 
 const Content: React.FC = () => (
   <main className="w-3/6">
     <Philosophies />
+    <Works />
   </main>
 );
 
