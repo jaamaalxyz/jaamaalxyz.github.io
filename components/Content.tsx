125a6cf037ca1badad0fba62471dff6d5d209af4
Md. Jamal Uddin <jamal.pb95@gmail.com>
2022-08-10 10:00:39 +0600

    code split to components

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
+import Philosophies from './Philosophies';
 
 const Content: React.FC = () => (
-  <main className="lg:w-5/8 p-5">
-    <p>I started off my career as a Planning Assistant</p>
-    <p>I transitioned to Network Engineering</p>
-    <p>I fell in love with Databases &amp; Data Analysis</p>
-    <p>We broke up</p>
-    <p>It was mutual</p>
-    <p>
-      I transitioned to Full Stack (React/Node.js) software development &amp;
-      never looked back
-    </p>
+  <main className="w-3/6">
+    <Philosophies />
   </main>
 );
 
