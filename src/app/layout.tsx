0b98125d3459c759d7ee89d9572bd139990b7d93
Md. Jamal Uddin <jaamaalxyz@gmail.com>
2024-02-11 19:43:39 +0600

    Add responsive design and update font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Merriweather } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const merriweather = Merriweather({ subsets: ['latin'], weight: '400' });
 
 export const metadata: Metadata = {
   title:
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={merriweather.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,23 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex max-w-4xl min-h-screen flex-col justify-center mx-auto">
+    <main className="flex max-w-4xl min-h-screen flex-col justify-center mx-auto p-10">
       <div>
-        <h1 className="font-bold text-9xl">Md. Jamal Uddin</h1>
-        <h3 className="font-bold text-md">
-          Full Stack Software Engineer - Building SaaS apps using React/React
-          Native & Expo
+        <h1 className="font-bold text-4xl md:text-6xl xl:text-7xl">
+          Md. Jamal Uddin
+        </h1>
+        <h3 className="mt-2">
+          <span className="font-bold text-sm md:text-md xl:text-xl 2xl:text-2xl">
+            Full Stack Software Engineer
+          </span>
+          {' - '}
+          <span className="text-sm md:text-md xl:text-xl 2xl:text-2xl">
+            Building Cross-Platform SaaS Products with React/React Native & Expo
+          </span>
         </h3>
       </div>
       <div className="mt-10">
-        <p>
+        <p className="text-sm md:text-md xl:text-xl">
           I am a self-taught experienced product engineer who specializes in
           building cross-platform mobile experiences using React, React Native &
           Expo. Leading a highly skilled mobile app development team that
@@ -21,12 +28,13 @@ export default function Home() {
         </p>
       </div>
 
-      <footer className=" flex mt-10">
+      <footer className="flex mt-10">
         <div className="flex gap-10">
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-sm md:text-md xl:text-xl 2xl:text-3xl"
           >
             Powered by{' '}
             <Image
@@ -42,6 +50,7 @@ export default function Home() {
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-sm md:text-md xl:text-xl 2xl:text-3xl"
           >
             Deployed on{' '}
             <Image
