65e635c7d07554fc3735516e4d2dca8d96af1a5f
Md. Jamal Uddin <jaamaalxyz@gmail.com>
2024-05-25 22:51:49 +0600

    Update footer UI

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -13,7 +13,7 @@ function FooterLinkGroup({
 }) {
   return (
     <div>
-      <p className="text-xl 2xl:text-5xl pb-5">{label}</p>
+      <p className="text-xl 2xl:text-5xl pb-5 text-emerald-300">{label}</p>
       <ul className="flex flex-col gap-2">
         {subLinks.map((link) => (
           <LinkItem key={link.label} title={link.label} link={link.link} />
@@ -27,36 +27,48 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="max-w-4xl mx-auto px-3 py-5  pt-16 pb-10 bg-slate-900">
-      <div className="flex justify-around">
-        {FOOTER_LINKS.map((link: FooterLink) => (
-          <FooterLinkGroup
-            key={link.label}
-            label={link.label}
-            subLinks={link.subLinks || []}
-          />
-        ))}
-      </div>
-      <div className="flex flex-col justify-center items-center border-t-2 border-slate-700 mt-10 px-10 py-5">
-        <p className="text-sm md:text-base xl:text-lg 2xl:text-xl">
-          Copyright {'©'} {currentYear} by{' '}
-          <Link href="/" className="text-sky-500 hover:text-sky-700">
-            Md. Jamal Uddin
-          </Link>
-        </p>
-        <p className="text-sm md:text-base xl:text-lg 2xl:text-xl">
-          Inspired by{' '}
-          <Link
-            href={'https://www.lekoarts.de/'}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sky-500 hover:text-sky-700"
-          >
-            Lennart
-          </Link>
-        </p>
-      </div>
+    <footer className="max-w-4xl mx-auto pt-20 bg-slate-900">
+      <FooterTop />
+      <FooterBottom currentYear={currentYear} />
     </footer>
+  );
+}
+
+const FooterTop = () => {
+  return (
+    <div className="flex justify-around pb-5">
+      {FOOTER_LINKS.map((link: FooterLink) => (
+        <FooterLinkGroup
+          key={link.label}
+          label={link.label}
+          subLinks={link.subLinks || []}
+        />
+      ))}
+    </div>
+  );
+};
+
+function FooterBottom({ currentYear }: { currentYear: number }) {
+  return (
+    <div className="flex flex-col justify-center items-center border-slate-700 mt-10 px-10 py-5 bg-slate-950">
+      <p className="text-sm md:text-base xl:text-lg 2xl:text-xl">
+        Copyright {'©'} {currentYear} by{' '}
+        <Link href="/" className="text-sky-500 hover:text-sky-700">
+          Md. Jamal Uddin
+        </Link>
+      </p>
+      <p className="text-sm md:text-base xl:text-lg 2xl:text-xl">
+        Inspired by{' '}
+        <Link
+          href={'https://www.lekoarts.de/'}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sky-500 hover:text-sky-700"
+        >
+          Lennart
+        </Link>
+      </p>
+    </div>
   );
 }
 
